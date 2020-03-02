6dae4d5e844fac3bdc0bca48e469cd044a8e3dc1
Farjaal Ahmad <farjaalahmad@gmail.com>
2020-03-02 19:54:12 +0500

    Update windows10_ONLY_RUN_THIS_FILE.bat

--- format_windows/windows10_ONLY_RUN_THIS_FILE.bat
+++ format_windows/windows10_ONLY_RUN_THIS_FILE.bat
@@ -1,3 +1,2 @@
 @echo off
 copy windows10_main.bat %userprofile%\appdata\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup\
-%userprofile%\appdata\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup\windows10_main.bat
