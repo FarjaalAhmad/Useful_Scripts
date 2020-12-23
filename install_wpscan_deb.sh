f1502f6a5b58cca37c1c0f075001b25f49f12ae4
Farjaal Ahmad <farjaalahmad@gmail.com>
2020-12-24 01:18:35 +0500

    Update install_wpscan_deb.sh

--- install_wpscan_deb.sh
+++ install_wpscan_deb.sh
@@ -1,8 +1,8 @@
 sudo apt-get install build-essential patch ruby ruby-dev zlib1g-dev liblzma-dev -y
 cd /opt
 sudo git clone https://github.com/wpscanteam/wpscan
-cd wpscan/
 sudo chown -R $(whoami):$(whoami) wpscan/
 sudo chmod -R 755 wpscan/
+cd wpscan/
 sudo gem install bundler
 sudo bundle install && sudo rake install
