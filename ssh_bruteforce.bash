9b43b405d35907d706081751a69959bd7bb7137d
Farjaal Ahmad <farjaalahmad@gmail.com>
2024-03-03 17:45:23 +0500

    Create ssh_bruteforce.bash

---
+++ ssh_bruteforce.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+usernames=$(< ssh_usernames.txt)
+ips=$(< inventory.txt)
+ports=$(< ssh_ports.txt)
+
+SSH_TIMEOUT=3
+
+for ip in $ips; do
+    for port in $ports; do
+        for username in $usernames; do
+            if ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking=no" -p "$port" -o "ConnectTimeout=$SSH_TIMEOUT" "$username@$ip" 'echo "Authentication successful!"' &> /dev/null; then
+                echo "Authentication successful for $username@$ip:$port"
+            else
+                echo "Authentication failed for $username@$ip:$port"
+            fi
+        done
+    done
+done
